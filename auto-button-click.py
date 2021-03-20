7c2cda9ed43dcb8bc1d543989ceffea009e1f0cf
tym21 <56368000+timknothe@users.noreply.github.com>
2021-03-20 14:10:16 +0100

    load config input parser basic start output

--- auto-button-click.py
+++ auto-button-click.py
@@ -2,11 +2,22 @@ import pyautogui
 import cv2
 import numpy
 import time
+import sys
 from configparser import ConfigParser
+
+config_language = "EN"
+config_refresh = 1
+WELCOME_TEXT = "CS:GO Auto Accept - V 1.0"
+INFO_TEXT = 'type "start" to run the program'
 
 
 def load_config():
-    pass
+    global config_language
+    global config_refresh
+    config = ConfigParser()
+    config.read("config.ini")
+    config_language = config["DEFAULT"]["language"]
+    config_refresh = config["DEFAULT"]["refresh"]
 
 
 def run():
@@ -14,11 +25,17 @@ def run():
 
 
 def parse_input(text: str):
-   pass
+    cmd = text.strip().lower()
+    if cmd == "start":
+        run()
+    elif cmd == "help":
+        help()
 
 
 def start():
-    pass
+    print(WELCOME_TEXT)
+    print(f"Config loaded: language = {config_language}; refresh = {config_refresh}")
+    print(INFO_TEXT)
 
 
 if __name__ == '__main__':
