8147dff77015b5aecd20dda7c76a9f09180de03c
tym21 <56368000+timknothe@users.noreply.github.com>
2021-03-20 02:31:10 +0100

    Basic structure

---
+++ auto-button-click.py
@@ -0,0 +1,28 @@
+import pyautogui
+import cv2
+import numpy
+import time
+from configparser import ConfigParser
+
+
+def load_config():
+    pass
+
+
+def run():
+    pass
+
+
+def parse_input(text: str):
+   pass
+
+
+def start():
+    pass
+
+
+if __name__ == '__main__':
+    load_config()
+    start()
+    while True:
+        parse_input(input("> "))
